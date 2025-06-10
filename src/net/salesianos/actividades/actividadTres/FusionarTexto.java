58e70400ae9f4e92cd5614a4d871d93d3d17c260
José Antonio Herrera Expósito <usr4034@salesianos-lacuesta.net>
2025-06-10 13:01:17 +0100

    feat: ej 3 apartado by c

--- src/net/salesianos/actividades/actividadTres/FusionarTexto.java
+++ src/net/salesianos/actividades/actividadTres/FusionarTexto.java
@@ -12,35 +12,29 @@ public class FusionarTexto {
         Path archivoTres = Paths.get("ficheros/actividadTres.txt");
         try {
             Files.createFile(archivoTres);
-            System.out.println("✔ Archivo creado: " + archivoTres.toAbsolutePath());
+            System.out.println("Archivo creado: " + archivoTres.toAbsolutePath());
         } catch (IOException e) {
-            System.err.println("✖ Error al crear el archivo: " + e.getMessage());
+            System.err.println("Error al crear el archivo: " + e.getMessage());
             return;
         }
 
-    //     // b) Fusionar contenido de actividad1.txt y actividad2.txt
-    //     Path archivoUno = Paths.get("ficheros/actividad1.txt");
-    //     Path archivoDos = Paths.get("ficheros/actividad2.txt");
-
-    //     try {
-    //         // Leer contenidos
-    //         String contenidoUno = Files.readString(archivoUno);
-    //         String contenidoDos = Files.readString(archivoDos);
-
-    //         // Escribir estructura fusionada
-    //         String estructuraFusionada = "- Contenido del Fichero Uno: " + contenidoUno + "\n" +
-    //                 "- Contenido del Fichero Dos: " + contenidoDos + "\n";
-
-    //         Files.writeString(archivoTres, estructuraFusionada, StandardOpenOption.WRITE);
-
-    //         // c) Añadir firma
-    //         String firma = "\nFirma: Fusionado por Actividad 3";
-    //         Files.writeString(archivoTres, firma, StandardOpenOption.APPEND);
-
-    //         System.out.println("✔ Contenidos fusionados en: " + archivoTres.toAbsolutePath());
-
-    //     } catch (IOException e) {
-    //         System.err.println("✖ Error al fusionar archivos: " + e.getMessage());
-    //     }
-    // }
+        // Fusionar contenido de actividad1.txt y actividad2.txt
+        Path archivoUno = Paths.get("ficheros/actividad1.txt");
+        Path archivoDos = Paths.get("ficheros/actividad2.txt");
+        try {
+            // Leer contenidos
+            String contenidoUno = Files.readString(archivoUno);
+            String contenidoDos = Files.readString(archivoDos);
+            // Escribir estructura fusionada
+            String estructuraFusionada = "- Contenido del Fichero Uno: " + contenidoUno + "\n" +
+                    "- Contenido del Fichero Dos: " + contenidoDos + "\n";
+            Files.writeString(archivoTres, estructuraFusionada, StandardOpenOption.WRITE);
+            // c) Añadir firma
+            String firma = "\nFirma: Fusionado por Actividad 3";
+            Files.writeString(archivoTres, firma, StandardOpenOption.APPEND);
+            System.out.println("✔ Contenidos fusionados en: " + archivoTres.toAbsolutePath());
+        } catch (IOException e) {
+            System.err.println("✖ Error al fusionar archivos: " + e.getMessage());
+        }
+    }
 }
