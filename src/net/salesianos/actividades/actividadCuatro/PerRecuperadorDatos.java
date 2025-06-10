13ccdcc947b571ea28ebb1b725c4b4d45ba9495e
José Antonio Herrera Expósito <usr4034@salesianos-lacuesta.net>
2025-06-10 13:28:13 +0100

    feat: añadimos la clase mesa con serialización

---
+++ src/net/salesianos/actividades/actividadCuatro/PerRecuperadorDatos.java
@@ -0,0 +1,10 @@
+package net.salesianos.actividades.actividadCuatro;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PerRecuperadorDatos {
+
+}
